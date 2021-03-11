2bff9def85c06341f30cf4eccebc3950bcea9953
novomestan <78145236+novomestan@users.noreply.github.com>
2021-03-11 17:23:11 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
